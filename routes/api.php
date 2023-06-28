5cb5d69fadb31d1e97b056ad43ab1669497e8da2
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-29 00:09:42 +0200

    FASE 1 - creo la gestione API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+// use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/* localhost:8000/api/test */
+Route::get('/projects', [ProjectController::class, 'index']);
